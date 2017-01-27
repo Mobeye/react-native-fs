7decb5e658b71b9562286450e5607cfeee339d87
Demis Borsalino <demis.borsalino@mobeye-app.com>
2017-01-27 10:19:27 +0100

    React method to download several files at the same time, first steps.

---
+++ android/src/main/java/com/rnfs/MultiDownloader.java
@@ -0,0 +1,161 @@
+package com.rnfs;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.HttpURLConnection;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import android.util.Log;
+
+import android.os.AsyncTask;
+
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
+
+public class MultiDownloader extends AsyncTask<MultiDownloadParams, int[], DownloadResult[]>{
+    private MultiDownloadParams mParam;
+    private AtomicBoolean mAbort = new AtomicBoolean(false);
+    private DownloadResult[] mDownloadResults;
+
+    protected DownloadResult[] doInBackground(MultiDownloadParams... params) {
+        mParam = params[0];
+
+        mDownloadResults = new DownloadResult[mParam.srcURLs.length];
+
+        try {
+            this.download(mParam);
+            mParam.onTaskCompleted.onTaskCompleted(mDownloadResults);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            mParam.onTaskCompleted.onTaskCompleted(mDownloadResults);
+            return mDownloadResults;
+        }
+
+        return mDownloadResults;
+    }
+
+    private void download(MultiDownloadParams downloadParams) {
+        InputStream input = null;
+        OutputStream output = null;
+        HttpURLConnection connection = null;
+
+        for (int i = 0; i < mDownloadResults.length; i++) {
+            try {
+                connection = (HttpURLConnection) downloadParams.srcURLs[i].openConnection();
+
+//                ReadableMapKeySetIterator iterator = param.headers.keySetIterator();
+//
+//                while (iterator.hasNextKey()) {
+//                    String key = iterator.nextKey();
+//                    String value = param.headers.getString(key);
+//                    connection.setRequestProperty(key, value);
+//                }
+
+                connection.setConnectTimeout(5000);
+                connection.setReadTimeout(15000);
+                connection.connect();
+
+                int statusCode = connection.getResponseCode();
+                int lengthOfFile = connection.getContentLength();
+
+                boolean isRedirect = (
+                        statusCode != HttpURLConnection.HTTP_OK &&
+                                (
+                                        statusCode == HttpURLConnection.HTTP_MOVED_PERM ||
+                                                statusCode == HttpURLConnection.HTTP_MOVED_TEMP
+                                )
+                );
+
+                if (isRedirect) {
+                    String redirectURL = connection.getHeaderField("Location");
+                    connection.disconnect();
+
+                    connection = (HttpURLConnection) new URL(redirectURL).openConnection();
+                    connection.setConnectTimeout(5000);
+                    connection.connect();
+
+                    statusCode = connection.getResponseCode();
+                    lengthOfFile = connection.getContentLength();
+                }
+
+                Map<String, List<String>> headers = connection.getHeaderFields();
+
+                Map<String, String> headersFlat = new HashMap<String, String>();
+
+                for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
+                    String headerKey = entry.getKey();
+                    String valueKey = entry.getValue().get(0);
+
+                    if (headerKey != null && valueKey != null) {
+                        headersFlat.put(headerKey, valueKey);
+                    }
+                }
+
+//                mParam.onDownloadBegin.onDownloadBegin(statusCode, lengthOfFile, headersFlat);
+
+                input = new BufferedInputStream(connection.getInputStream(), 8 * 1024);
+                output = new FileOutputStream(downloadParams.destFiles[i]);
+
+                byte data[] = new byte[8 * 1024];
+                int total = 0;
+                int count;
+                double lastProgressValue = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    if (mAbort.get()) throw new Exception("Download has been aborted");
+
+                    total += count;
+//                    if (param.progressDivider <= 0) {
+//                        publishProgress(new int[]{lengthOfFile, total});
+//                    } else {
+//                        double progress = Math.round(((double) total * 100) / lengthOfFile);
+//                        if (progress % param.progressDivider == 0) {
+//                            if ((progress != lastProgressValue) || (total == lengthOfFile)) {
+//                                Log.d("Downloader", "EMIT: " + String.valueOf(progress) + ", TOTAL:" + String.valueOf(total));
+//                                lastProgressValue = progress;
+//                                publishProgress(new int[]{lengthOfFile, total});
+//                            }
+//                        }
+//                    }
+                    output.write(data, 0, count);
+                }
+
+                output.flush();
+
+                // Bizarrement je n'arrive pas à écrire là-dedans --> NullPointer exception
+                mDownloadResults[i] = new DownloadResult();
+                mDownloadResults[i].statusCode = statusCode;
+                mDownloadResults[i].bytesWritten = total;
+            } catch(Exception e) {
+                Log.d("RNFS MultiDownloader", "Exception in download number " + Integer.toString(i));
+                mDownloadResults[i].exception = e;
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (output != null) {
+                        output.close();
+                        output = null;
+                    }
+                    if (input != null) {
+                        input.close();
+                        input = null;
+                    }
+                    if (connection != null) {
+                        connection.disconnect();
+                        connection = null;
+                    }
+                } catch(Exception e) {
+                    Log.d("RNFS MultiDownloader", "Exception in finally");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
